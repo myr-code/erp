8c5679d368b2ec22266f1416bf8ee22b9905fa23
myr <9527@qq.com>
2021-11-26 18:27:23 +0800

    B座20F 报表模板测试javabean

--- src/main/java/com/myr/Controller/report/reportController.java
+++ src/main/java/com/myr/Controller/report/reportController.java
@@ -1,6 +1,10 @@
 package com.myr.Controller.report;
 
+import com.myr.Bean.User;
+import com.myr.Service.UserService;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -15,15 +19,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @Scope("prototype")
 public class reportController {
+    @Autowired
+    UserService userService;
 
-
-    //序时簿 高级查询
+    //基于JDBC连接的方式展示PDF
     @RequestMapping("/report_test")
     public void report_test(HttpServletRequest request, HttpServletResponse response)throws Exception {
 
@@ -36,11 +43,7 @@ public class reportController {
 
         try {
             Map pars = new HashMap<>();
-            /*pars.put("id","1");
-            pars.put("name","东鸿");
-            pars.put("age","18");
-            pars.put("sg","172");
-            pars.put("phone","15900000003");*/
+            pars.put("fid",2);
             Connection conn = getConnection();
             /*JasperPrint print = JasperFillManager.fillReport(fis, pars, new JREmptyDataSource());*/
             JasperPrint print = JasperFillManager.fillReport(fis, pars, conn);
@@ -52,6 +55,40 @@ public class reportController {
         }
 
     }
+
+    //基于JDBC连接的方式展示PDF
+    @RequestMapping("/report_javabean")
+    public void report_javabean(HttpServletRequest request, HttpServletResponse response)throws Exception {
+
+        //1、引入jasper文件
+        Resource resource = new ClassPathResource("templates/report_template/testJavaBean.jasper");
+        FileInputStream fis = new FileInputStream(resource.getFile());
+
+        //2、创建JasperPrint,向jasper文件中填充数据
+        ServletOutputStream os = response.getOutputStream();
+
+        try {
+            Map pars = new HashMap<>();
+
+            User user = new User();
+            user.setEmail("admin");
+            user.setPassword("9527");
+            List<User> list = new ArrayList<>();
+            User dbuser = userService.getUserByName(user);
+            list.add(dbuser);
+
+            JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(list);
+            /*JasperPrint print = JasperFillManager.fillReport(fis, pars, new JREmptyDataSource());*/
+            JasperPrint print = JasperFillManager.fillReport(fis, pars, ds);
+            JasperExportManager.exportReportToPdfStream(print,os);
+        }catch (JRException e){
+            e.printStackTrace();
+        }finally {
+            os.flush();
+        }
+
+    }
+
 
     //连接数据库
     public Connection getConnection() throws Exception{
