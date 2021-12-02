64d7c962d058d3247b8c9c623db453f83a859a0e
myr <9527@qq.com>
2021-12-02 18:25:13 +0800

    更新地点：B座20F 更新完成内容：无 下一步更新计划：被引用不能删除(已更新到删除销售订单)、修改本地MySQL密码 其他：无

--- src/main/java/com/myr/utils/DBConn.java
+++ src/main/java/com/myr/utils/DBConn.java
@@ -8,11 +8,13 @@ public class DBConn extends Exception{
     //连接数据库
     public static Connection getConnection() throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
-        //test
+        /*//test
         Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/myerp", "root", "123456");
-        /*//云服务器
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/myerp", "root", "erp2021");
         */
+
+        //云服务器
+        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/myerp", "root", "erp2021");
+
         return conn;
     }
 }
