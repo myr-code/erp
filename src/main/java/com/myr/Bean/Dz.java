7ebec47fe3d1764271da193b0b9d2003a7f05f59
myr <9527@qq.com>
2021-12-14 18:33:29 +0800

    更新地点：E 40F 更新完成内容：销售出库add、bomadd 下一步更新计划：bom edit 其他：无

---
+++ src/main/java/com/myr/Bean/Dz.java
@@ -0,0 +1,50 @@
+package com.myr.Bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dz {
+
+  private int fid;
+  private String billNo;
+  private String billDate;
+  private String mrpNo;
+  private int custId;
+  private String currencyName;
+  private String address;
+  private String contact;
+  private String phone;
+  private String settleName;
+  private String remark;
+  private int billYear;
+  private int billPeriod;
+  private int itemId;
+  private int stockId;
+  private String unitName;
+  private String saleOrderBillNo;
+  private double saleOrderQty;
+  private double saleOutQty;
+  private double taxPriceNo;
+  private String skph;
+  private String voucheNumber;
+  private double ykpAmt;
+  private double wkpAmt;
+  private double yjsAmt;
+  private double wjsAmt;
+  private int sourFid;
+  private String sourBillNo;
+  private int sourEntry;
+  private String sourType;
+  private int dzCheckId;
+  private String dzCheckDate;
+  private int billStaffId;
+  private int billCheckId;
+  private String createDate;
+  private String editDate;
+
+
+}
